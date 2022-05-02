93b0f4a69f66f30ba2fd0ff9f01b60addc867c71
Khayman King <khaymanak@gmail.com>
2022-05-02 12:59:04 -0700

    saving before lunch, also i hate forms

--- src/City.js
+++ src/City.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function City(props) {
+export default function City({ waterfrontId, skyLineId, castleID }) {
   return (
     <div className="city">
       {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
